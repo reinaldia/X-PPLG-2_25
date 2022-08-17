18e36241f0e173b7cb1c937a5697d5b8dfdfa805
reinaldia <111436059+reinaldia@users.noreply.github.com>
2022-08-17 20:48:19 +0700

    Add files via upload
    Revisi

---
+++ index.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    document.body.classList.toggle('disablescroll')
+}); 
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+window.onload = function() {
+    document.getElementById("my_audio").play();
+}
+
+$(document).ready(function(){
+
+$("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+    event.preventDefault();
+
+    
+    var hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function(){
+        window.location.hash = hash;
+    });
+    }
+});
+});
