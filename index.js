07bc2fea2260a635efd16b59f85e66770b39ef00
reinaldia <ghazi.ghifari25@gmail.com>
2022-08-25 07:03:42 +0700

    Update 1.1

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+/* navbar sticky */
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
